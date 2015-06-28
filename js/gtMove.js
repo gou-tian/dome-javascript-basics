aa81b64902977f0687c7931a6723d62657096a49
gousky <gousky@126.com>
2015-06-28 20:00:56 +0800

    匀速运动以json形式完成。带修复Bug为不能随意传入速度值 匀速运动带完成内容：以json形式完成参数的属性(attr)和目标值(tar)的传递 (JavaScript)
    完成37个练习
    DOM 元素拖拽35-37
    35、元素拖拽基础
    36、元素拖拽重叠检测（元素碰撞检测）
    37、元素拖拽限制范围
    添加运动函数（gtMove）和抖动函数（gtShake）以及获取元素和元素属性函数（gtStyle）
    第11个需要间隔一段时间在重做，加以巩固。
    函数传参练习完成，2015/05/28第21个动画没有结束前，阻止点击事件有BUG。
    2015/05/30修复第21个动画BUG，第22个需要复习巩固,第23个暂时不可重复点击，第24\25个需要复习，加强记忆.
    字符串操作内容需要加强记忆和练习。
    (注：谨记！如有多个相同的功能，可以先做出来一个，其他的用传参或是循环实现。)

---
+++ js/gtMove.js
@@ -0,0 +1,66 @@
+/**
+ * Created by gousky on 2015/6/28.
+ *
+ * 匀速运动
+ *
+ *json形式出入传入属性(attr)和属性值或是目标值(target)
+ */
+//定义参数obj(传入的元素),json传入属性(attr)和属性值或是目标值(target),num(速度计算值),fn(回调函数))
+function $gt( obj,json,iSpeed,fn ){
+    //关闭定时器
+    clearInterval(obj.iTimer);
+    //初始化一个新的变量，赋值为数字类型
+    var num = 0;
+    //开启定时器
+    obj.iTimer = setInterval(function(){
+        //定义新的变量判断多属性值是否到达预定目标值,变量类型为布尔值
+        var spdTar = true;
+        //循环json
+        for(var attr in json){
+            //声明一个变量存放目标值
+            var iTarget = json[attr];
+            //判断attr属性名称，如果是opacity用Math取整
+            if(attr == 'opacity'){
+                num = Math.round(gtCss(obj,'opacity') * 100);
+            }else{
+                num = parseInt(gtCss(obj,attr));
+            }
+
+            if(num > iTarget && num > 0 || num < iTarget && num < 0){
+                num = iTarget;
+            }
+
+            //如果num>iTarget的时候num=iTarget
+            if(num != iTarget){
+                spdTar = false;
+                if (attr == 'opacity') {
+                    obj.style.opacity = (num + iSpeed) / 100;
+                    obj.style.filter = 'alpha(opacity='+ (num + iSpeed) +')';
+                } else {
+                    obj.style[attr] = num + iSpeed + 'px';
+                }
+            }
+
+        }
+        if(spdTar){
+            num = iTarget;
+            clearInterval(obj.iTimer);
+            console.log('End');
+            fn && fn.call(obj);
+        }
+    },30)
+}
+
+//获取css样式attr属性值并做浏览器兼容处理
+function gtCss(obj,attr){
+    //obj(元素),attr(属性名称)
+    //处理浏览器兼容性问题
+    if(obj.currentStyle){
+        //currentStyle 做IE非标准浏览器兼容处理
+        return obj.currentStyle[attr];
+    }else{
+        //getComputedStyle FF等标准浏览器兼容处理
+        return getComputedStyle(obj)[attr];
+    }
+}
+//TODO:匀速运动以json形式完成。带修复Bug为不能随意传入速度值
